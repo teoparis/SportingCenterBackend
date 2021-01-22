bca2fe7327bcd816a16ba77e50014a585cc60a4c
santealtamura <sante_altamura97@hotmail.com>
2021-01-22 21:46:05 +0100

    Aggiunta controller calendario

--- calendar-service/src/main/java/com/sportingCenterWebApp/calendarservice/controller/EventController.java
+++ calendar-service/src/main/java/com/sportingCenterWebApp/calendarservice/controller/EventController.java
@@ -1,5 +1,28 @@
 package com.sportingCenterWebApp.calendarservice.controller;
 
-public class EventController {
+import com.sportingCenterWebApp.calendarservice.model.Event;
+import com.sportingCenterWebApp.calendarservice.repo.EventRepository;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class EventController {
+    private final EventRepository eventRepository;
+
+    public EventController(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @GetMapping("/events")
+    public List<Event> getActivities() {
+
+        return (List<Event>) eventRepository.findAll();
+    }
+
+    @PostMapping("/event")
+    void addActivity(@RequestBody Event newEvent){
+        eventRepository.save(newEvent);
+    }
 }
--- calendar-service/src/main/java/com/sportingCenterWebApp/calendarservice/model/Event.java
+++ calendar-service/src/main/java/com/sportingCenterWebApp/calendarservice/model/Event.java
@@ -5,8 +5,6 @@ import java.util.Date;
 
 @Entity
 @Table(name = "Event")
-
-
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,10 +17,13 @@ public class Event {
     @Column(name = "end")
     private Date dataFine;
 
+    @Column(name = "color")
+    private String color;
+
     @Column(name = "title")
     private String title;
 
-    @Column(name = "allDay")
-    private Boolean allDay;
+    @Column(name = "ACT_ID")
+    private long activity_id;
 
 }
