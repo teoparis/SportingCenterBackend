3526bee260fd93e7983c8411d0e0350029437f49
santealtamura <sante_altamura97@hotmail.com>
2021-02-16 10:56:37 +0100

    Aggiunta CORS configuration

--- zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/ZuulServerApplication.java
+++ zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/ZuulServerApplication.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @SpringBootApplication
@@ -22,6 +25,24 @@ public class ZuulServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
 
